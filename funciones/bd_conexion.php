6e53d9aa4b5e80707779730f8b5dda4e68883250
Alex <alexsan.freelancer@gmail.com>
2022-08-05 10:52:57 -0300

    Fixed conection file User/Password

--- funciones/bd_conexion.php
+++ funciones/bd_conexion.php
@@ -1,7 +1,7 @@
 <?php
     
-    $user = 'sisconsy_admin';
-    $password = 'sis25/33?con';
+    $user = 'admin_siscon';
+    $password = 'alex3344/';
     $db = 'sisconsy_admin_data';
     $host = 'localhost';
     
